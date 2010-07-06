418138d024cb77085baec647ef1b7b4e401beeaa
zoomika <szyszo3@freemail.hu>
2010-07-06 16:57:45 +0200

    Currently unused

---
+++ EventAI_TEST/src/scripts/eventai/eventai.h
@@ -0,0 +1,41 @@
+/* ########################
+## BBCore EventAI system ##
+######################## */
+
+#ifndef DEF_EVENTAI_H
+#define DEF_EVENTAI_H
+
+enum eSays
+{
+	SAY_VERMIN1		= -1740000,
+	SAY_VERMIN2		= -1740001,
+	SAY_DEFIAS1		= -1740002,
+	SAY_DEFIAS2		= -1740003,
+	SAY_DEFIAS3		= -1740004,
+	SAY_FLEE		= -1740005,
+	SAY_RIVER1		= -1740006,
+	SAY_RIVER2		= -1740007,
+	SAY_PADFOOT		= -1740008,
+};
+
+enum eSpells
+{
+	SPELL_PIERCE	= 6016, 	// Pierce Armor
+	SPELL_POISON	= 11918, 	// Poison
+	SPELL_POTION	= 3368, 	// Drink Minor Potion
+	SPELL_HARMST	= 9080,		// Harmstring
+	SPELL_BATTLE	= 7165,		// Battle Stance[rank 1]
+	SPELL_NET	= 12024,	// Net
+	SPELL_DISEASE	= 71764,	// Disease
+	SPELL_BSHOUT	= 9128,		// Battle Shout
+	SPELL_BSTAB	= 53,		// Backstab[rank 1]
+	SPELL_THROW	= 10277,	// Throw
+	SPELL_QBLUST	= 3229,		// Quick Bloodlust
+	SPELL_GOUGE	= 1776,		// Gouge
+	SPELL_THRASH	= 3391,		// Thrash
+	SPELL_DEFENS	= 7164,		// Defensive Stance
+	SPELL_CHARGE	= 6268,		// Rushing Charge
+	SPELL_WIDES	= 7342,		// Wide Slash 
+};
+
+#endif
