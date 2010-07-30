3c79a75b9f2de6129d5d7df140f0a27fa01cd71b
zoomika <szyszo3@freemail.hu>
2010-07-30 19:23:59 +0200

    update duke vallenhal's position

---
+++ updates/0011_creature.sql
@@ -0,0 +1,4 @@
+-- Update Duke Vallenhal's position
+UPDATE `creature` SET `position_x`=3637.10098,`position_y`=966.552979,`position_z`=56.311298,`orientation`=4.724670 WHERE `id`=26926;
+
+UPDATE `version` SET `db_version`='BBDB_0011_07301920';
