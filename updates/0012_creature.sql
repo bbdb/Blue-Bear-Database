5aa65d449662b89f272d2849d49fed7c9e87919f
zoomika <szyszo3@freemail.hu>
2010-07-30 19:29:36 +0200

    fix typo

---
+++ updates/0012_creature.sql
@@ -0,0 +1,4 @@
+-- Fix typo
+UPDATE `creature` SET `position_x`='3658.014404',`position_y`='1000.173523',`position_z`='55.779942',`orientation`='4.510347' WHERE `id`=26926;
+
+UPDATE `version` SET `db_version`='BBDB_0012_07301928';
